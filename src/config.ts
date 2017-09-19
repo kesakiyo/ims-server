540568788d6fd3a112961424135641b724f44bf1
mino <kesakiyo@naver.com>
2017-09-20 04:11:26 +0900

    Delete config.ts from repo

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export default {
-  mongoURL: 'mongodb://kesakiyo:JngDCruRDMvCdtY4@ims-mongodb-shard-00-00-kkhey.mongodb.net:27017,ims-mongodb-shard-00-01-kkhey.mongodb.net:27017,ims-mongodb-shard-00-02-kkhey.mongodb.net:27017/test?ssl=true&replicaSet=ims-mongodb-shard-0&authSource=admin'
-}
