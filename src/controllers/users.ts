1842900c6a4189a60ef490def6a15fafeb4a69de
MinhoCheon <mino@MinhoCheonui-MacBook-Pro.local>
2017-10-12 03:51:08 +0900

    Add passport local(need to test)

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,13 @@
 /* External dependencies */
 import * as express from 'express';
 import { Router, Request, Response, NextFunction } from 'express';
-import { Document } from 'mongoose'
-import * as HttpStatus from 'http-status-codes'
+import { Document } from 'mongoose';
+import * as HttpStatus from 'http-status-codes';
+import * as passport from 'passport';
+import { LocalStrategyInfo } from 'passport-local';
 
 /* Internal dependencies */
-import User from '../models/User'
+import { default as User, UserModel } from '../models/User';
 
 const router: Router = express.Router();
 
@@ -40,6 +42,28 @@ router.post('/', (req: Request, res: Response, next: NextFunction): void => {
         return next(err);
       }
       res.status(HttpStatus.OK).send(user.toJSON())
+    })
+  })
+});
+
+router.post('/signin', (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('local', (err: Error, user: UserModel, info: LocalStrategyInfo) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.status(HttpStatus.UNPROCESSABLE_ENTITY).send({
+        error: 'Not found user',
+      });
+    }
+
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.status(200).json(user);
     })
   })
 });
--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,28 @@ import * as express from 'express';
 import { Express, Router } from 'express';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
+import * as session from 'express-session';
 
 /* Internal dependencies */
 import ping from './controllers/ping';
 import users from './controllers/users';
-import config from './config';
+import secret from './config/secret';
 
+/* API keys and Passport configuration. */
+import * as passportConfig from './config/passport';
+
+/* Create Express Server */
 const app: Express = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+}));
 
-mongoose.connect(config.MONGO_URL, { useMongoClient: true, promiseLibrary: global.Promise }, (err): void => {
+mongoose.connect(secret.MONGO_URL, { useMongoClient: true, promiseLibrary: global.Promise }, (err): void => {
   if (err) {
     console.log('Occurred the error when connecting mongodb: ', err);
   }
