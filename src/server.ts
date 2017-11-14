f3321aaf49c6a82d118b1a184b3658200925f709
MinhoCheon <mino@MinhoCheonui-MacBook-Pro.local>
2017-11-15 00:30:42 +0900

    add options prop to cors

--- src/server.ts
+++ src/server.ts
@@ -53,6 +53,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({
   origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
 
